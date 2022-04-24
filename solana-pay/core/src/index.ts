d51563822785c2ff96fe8c3d4e6b7ec5acc1ad95
Greeshma D <greeshma@Greeshmas-MacBook-Pro.local>
2022-04-24 22:38:02 +0530

    deleted solana pay folder

--- solana-pay/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './constants';
-export * from './createQR';
-export * from './createTransaction';
-export * from './encodeURL';
-export * from './findTransactionSignature';
-export * from './parseURL';
-export * from './validateTransactionSignature';
